0f42d908518583abff9d2f456ca9d852e9bab0af
shavez <shvezahmed9966@gmail.com>
2023-07-03 11:09:53 +0530

    new turtle

---
+++ t1.py
@@ -0,0 +1,18 @@
+from turtle import *
+speed('slowest')
+fd(150)#forward
+rt(60)
+fd(150)#forward
+rt(60)
+fd(150)#forward
+rt(60)
+fd(150)#forward
+rt(60)
+fd(150)#forward
+rt(60)
+fd(150)#forward
+rt(60)
+fd(150)#forward
+rt(60)
+
+mainloop()#this is used to hold the window
